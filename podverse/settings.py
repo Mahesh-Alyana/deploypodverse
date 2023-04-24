cf5a5cc49592fc391d83cb2eef652be7504d5027
akash1331 <akashpk1331@gmail.com>
2023-04-24 15:25:36 +0530

    corsheaders

--- podverse/settings.py
+++ podverse/settings.py
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'django_filters',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -56,6 +58,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'podverse.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3030',
+]
+
 
 TEMPLATES = [
     {
